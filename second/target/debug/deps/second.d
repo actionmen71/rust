339398604d7e46dc4f534c97eac2925186141492
KS <actionmen71@gmail.com>
2024-10-22 09:01:02 +0530

    second commit

---
+++ second/target/debug/deps/second.d
@@ -0,0 +1,5 @@
+C:\Users\actio\Desktop\rustlings-tut\second\target\debug\deps\second.exe: src/main.rs
+
+C:\Users\actio\Desktop\rustlings-tut\second\target\debug\deps\second.d: src/main.rs
+
+src/main.rs:
---
+++ second/target/debug/second.d
@@ -0,0 +1,1 @@
+C:\Users\actio\Desktop\rustlings-tut\second\target\debug\second.exe: C:\Users\actio\Desktop\rustlings-tut\second\src\main.rs
