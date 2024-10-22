d82153c9fd5b08000b586192b247f55385cfa8be
KS <actionmen71@gmail.com>
2024-10-22 08:25:10 +0530

    first commit

---
+++ 1/target/debug/deps/rust_practice-d264f6b2c41a9a64.d
@@ -0,0 +1,5 @@
+C:\Users\actio\Desktop\rustlings-tut\rust-practice\target\debug\deps\librust_practice-d264f6b2c41a9a64.rmeta: src/main.rs
+
+C:\Users\actio\Desktop\rustlings-tut\rust-practice\target\debug\deps\rust_practice-d264f6b2c41a9a64.d: src/main.rs
+
+src/main.rs:
---
+++ 1/target/debug/deps/rust_practice-fbf7d98822f7f2f6.d
@@ -0,0 +1,5 @@
+C:\Users\actio\Desktop\rustlings-tut\rust-practice\target\debug\deps\librust_practice-fbf7d98822f7f2f6.rmeta: src/main.rs
+
+C:\Users\actio\Desktop\rustlings-tut\rust-practice\target\debug\deps\rust_practice-fbf7d98822f7f2f6.d: src/main.rs
+
+src/main.rs:
